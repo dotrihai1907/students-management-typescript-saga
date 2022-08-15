216d177aad7f4808aba22a8db3447ed1c33cee16
Đỗ trí Hải <dotrihai1999@gmail.com>
2022-08-15 14:01:19 +0700

    routing Student managament

--- src/features/student/index.tsx
+++ src/features/student/index.tsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/material";
 import * as React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import AddEditPage from "./components/AddEditPage";
+import ListPage from "./components/ListPage";
 
 export default function StudentFeature() {
-  return <div>Student Feature</div>;
+  const match = useRouteMatch();
+  return (
+    <Switch>
+      <Route path={match.path} exact component={ListPage} />
+      <Route path={`${match.path}/add`} component={AddEditPage} />
+      <Route path={`${match.path}/:studentId`} component={AddEditPage} />
+    </Switch>
+  );
 }
